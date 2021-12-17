89f7e595a33794f66bb08d2bb11fb0f1e343fc6f
drklbri <drklibr@gmail.com>
2021-12-17 08:51:33 +0300

    123

--- src/ru/vsu/cs/shevchenko_daniil/MaxSumRange.java
+++ src/ru/vsu/cs/shevchenko_daniil/MaxSumRange.java
@@ -1,5 +1,6 @@
 package ru.vsu.cs.shevchenko_daniil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MaxSumRange {
@@ -11,22 +12,40 @@ public class MaxSumRange {
         this.sum = sum;
     }
 
-    public static MaxSumRange ListIndex(List<Integer> list) {
+    public static List<Integer> ListIndex(List<Integer> list) {
         int currSum = 0;
         int startIndex = 0;
         MaxSumRange maxSumRange = null;
+        int maxSum = maxSumOfSubArray(list);
+        List<Integer> tempList = new ArrayList<>();
 
         for (int endIndex = 0; endIndex < list.size(); endIndex++) {
             currSum += list.get(endIndex);
 
-            if (maxSumRange == null || currSum > maxSumRange.sum)
+            if (maxSumRange == null || currSum > maxSumRange.sum) {
                 maxSumRange = new MaxSumRange(startIndex, endIndex, currSum);
+            }
 
             if (currSum < 0) {
                 currSum = 0;
                 startIndex = endIndex + 1;
             }
+            if (currSum == maxSum) {
+                tempList.add(startIndex);
+                tempList.add(endIndex);
+            }
         }
-        return maxSumRange;
+        return tempList;
     }
+
+    public static int maxSumOfSubArray(List<Integer> list) {
+        int currSum = 0;
+        int maxSum = 0;
+        for (int number : list) {
+            currSum = Math.max(0, number+currSum);
+            maxSum = Math.max(currSum,maxSum);
+        }
+        return maxSum;
+    }
+
 }
